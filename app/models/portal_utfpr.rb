d093689b224d782af4306f15ff9c9cd14acc117b
Orochi <imatheusfsantos@gmail.com>
2017-05-03 00:28:43 -0300

    adicionar gem certified pra tentar resolver problema de SSL

--- app/models/portal_utfpr.rb
+++ app/models/portal_utfpr.rb
@@ -1,7 +1,21 @@
 class PortalUtfpr
-  def self.logar(credencial, login: nil, senha: nil)
-
+  LOGIN_URL = ""
+  def self.logar(login: , senha:)
+    agent = Mechanize.new{|a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE}
+    agent.ssl_version = 'SSLv3'
+    agent.add_auth(PortalUtfpr.login_url(9), login, senha)
+    agent.get(PortalUtfpr.login_url(9)) do |page|
+      puts page.title
+    end
     ConexaoUtfpr.new
-      
+
+  end
+
+  def self.login_url(cod_campus)
+    cod_campus
+    cod = ""
+    cod += "0" if cod_campus <= 9
+    cod += "#{cod_campus}"
+    "http://utfws.utfpr.edu.br/aluno#{cod}/sistema/mpmenu.inicio"
   end
 end
--- test/models/aluno_test.rb
+++ test/models/aluno_test.rb
@@ -4,26 +4,32 @@ class AlunoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @RA = Rails.application.secrets.RA_VALIDO_1
+    @password = Rails.application.secrets.SENHA_VALIDA_1
 
+    @RA_trancado = Rails.application.secrets.RA_TRANCADO_1
+    @password_trancado = Rails.application.secrets.SENHA_TRACADO_1
+
+
+  end
   test "logar no portal da UTFPR com login válido" do
-    credencial = Credencial.new("1602632", "jefferson123")
-    connection = PortalUtfpr.logar(credencial)
+    connection = PortalUtfpr.logar(login: @RA, senha: @password)
     assert connection.logado?
   end
   test "logar no portal da UTFPR com login inválido" do
-    credencial = Credencial.new("1602632", "jefferson123")
+    credencial = Credencial.new(@RA, "kk*8(*)*)")
     connection = PortalUtfpr.logar(credencial)
     assert_not connection.logado?
   end
 
   test "logar no portal da UTFPR sem a classe Credencial" do
-    connection = PortalUtfpr.logar(RA: "1602632", senha: "jefferson123")
-    assert_not connection.logado?
+    connection = PortalUtfpr.logar(login: @RA, senha: @password)
+    assert connection.logado?
   end
 
   test "logar no portal da UTFPR com matricula trancada" do
-    credencial = Credencial.new("1602632", "jefferson123")
-    connection = PortalUtfpr.logar(credencial)
+    connection = PortalUtfpr.logar(login: @RA_trancado, senha: @password)
     assert connection.logado?
     aluno = connection.build_aluno
     matricula = aluno.matricula_cursos.first
