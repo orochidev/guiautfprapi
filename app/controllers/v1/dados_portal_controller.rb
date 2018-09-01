86cc83e7180c5f462fd8994f18356f27570ce3a1
imatheusfsantos@gmail.com <imatheusfsantos@gmail.com>
2018-09-01 02:10:47 -0300

    p

--- app/controllers/v1/dados_portal_controller.rb
+++ app/controllers/v1/dados_portal_controller.rb
@@ -16,24 +16,10 @@ module V1
     # POST /alunos
     # POST /alunos.json
 
+    def next_class
 
-    private
-    def openstruct_to_hash(object, hash = {})
-    object.each_pair do |key, value|
-      hash[key] = value.is_a?(OpenStruct) ? openstruct_to_hash(value) : value
     end
-    hash
-  end
 
 
-      # Use callbacks to share common setup or constraints between actions.
-      def set_aluno
-        @aluno = Aluno.find(params[:id])
-      end
-
-      # Never trust parameters from the scary internet, only allow the white list through.
-      def aluno_params
-        params.fetch(:aluno, {})
-      end
   end
 end
