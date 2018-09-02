049fde0d417fc309d5b56831c9154fe25e2c20b3
orochidev <orochidev@gmail.com>
2018-09-02 07:57:01 -0300

    Dockerizando a parada

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :alunos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #API
+  #root 'topics#index'
  namespace :v1, defaults: { format: :json }  do
 
    get "/dados_portal" => "dados_portal#show" #GET usuarios/logar?
