ceb697ec407c740f6a0807f0e2b9fe327cf305d0
orochidev <orochidev@gmail.com>
2018-10-05 01:40:57 -0300

    Modificacoes para capturar dados de alunos que ja fizeram + de 1 curso

--- app/models/pagina_inicial_utfpr.rb
+++ app/models/pagina_inicial_utfpr.rb
@@ -11,30 +11,54 @@ class PaginaInicialUTFPR < PaginaUTFPR
     str = aux.text
     hash.RA = self.conexao.RA
     hash.nome = str.match(/[^\d -][\W\w]+/)[0]
+    if !html.at_css("select#p_CursLinha")
+      #Para extrair nome do curso e situação
+      aux = html.at("//*[@id='cursos']/center/font")
+      aux.search(".//strong").remove #bang
+      str = aux.text.strip
+      hash.nome_curso = str.split("-")[0].strip
+      hash.situacao_curso = str.split("-")[1].strip
 
-    #Para extrair nome do curso e situação
-    aux = html.at("//*[@id='cursos']/center/font")
-    aux.search(".//strong").remove #bang
-    str = aux.text.strip
-    hash.nome_curso = str.split("-")[0].strip
-    hash.situacao_curso = str.split("-")[1].strip
+      #Pra pegar umas variavel louca ai que não sei pra que serve
+      aux = html.search("script:contains('jsCursos')").last
+      str = aux.text.match(/{[\w\W]*}/)[0].gsub(/\s+/, "").gsub(/([a-z]+):/, '"\1":')
 
-    #Pra pegar umas variavel louca ai que não sei pra que serve
-    aux = html.search("script:contains('jsCursos')").last
-    str = aux.text.match(/{[\w\W]*}/)[0].gsub(/\s+/, "").gsub(/([a-z]+):/, '"\1":')
+      json = JSON.parse(str)
 
-    json = JSON.parse(str)
+      hash.linha = json["linha"]
+      hash.curscodnr = json["curscodnr"]
+      hash.tpcurcodnr = json["tpcurcodnr"]
+      hash.alcuordemnr = json["alcuordemnr"]
+      hash
+    else
+      aux = html.search("select#p_CursLinha option:contains('Regular')").first
+      linha = aux.attr("value")
 
-    hash.linha = json["linha"]
-    hash.curscodnr = json["curscodnr"]
-    hash.tpcurcodnr = json["tpcurcodnr"]
-    hash.alcuordemnr = json["alcuordemnr"]
-    hash
+      hash.nome_curso = aux.text.split("-")[0].strip
+      hash.situacao_curso = aux.text.split("-")[2].split(":")[1].strip
+      p hash.nome_curso
+      p hash.situacao_curso
+
+      aux = html.search("script:contains('jsCursos')").last
+      str = aux.text.split("=")[1].split(";")[0].chomp.strip.gsub(/([a-z]+):/, '"\1":')
+
+      json = JSON.parse(str)
+
+      json = json.select{|j| j["linha"].to_s == linha}.first
+
+      hash.linha = json["linha"]
+      hash.curscodnr = json["curscodnr"]
+      hash.tpcurcodnr = json["tpcurcodnr"]
+      hash.alcuordemnr = json["alcuordemnr"]
+      hash
+    end
   end
   private
   def page_url
     "https://utfws.utfpr.edu.br/aluno#{cod_campus}/sistema/mpmenu.inicio"
 
   end
+  def has_select_box?
 
+  end
 end
